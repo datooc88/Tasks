bbd909482af40eb336a23a5b3dfac2ab15fc0133
datooc88 <51028249+datooc88@users.noreply.github.com>
2019-05-27 19:49:14 +0300

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace davidocTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        string repos = "https://api.github.com/search/repositories?q=";
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult saveBM(string bm)
+        {
+            var response = "true";
+            Session.Add("Bookmark",bm);
+            return Json(response);
+        }
+
+    }
+}
